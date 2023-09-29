a076f7ee4dfee41c52e7ec8692633ee694cfafd8
milkyfuckinway <0-0_0@bk.ru>
2023-09-29 19:07:49 +0300

    add resize

--- src/features/Window/Window.tsx
+++ src/features/Window/Window.tsx
@@ -1,7 +1,7 @@
 import { increaceZIndex, setFileActive } from '@/store/files.slice';
 import { useAppSelector } from '@/store/store';
 import clsx from 'clsx';
-import { motion, useDragControls } from 'framer-motion';
+import { motion, useDragControls, useMotionValue, useTransform } from 'framer-motion';
 import { useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
@@ -10,16 +10,28 @@ import TitleBar from './TitleBar';
 import styles from './Window.module.scss';
 
 export default function Window({ item }: { item: Item }) {
-  const [expanded, setExpanded] = useState(false);
   const dispatch = useDispatch();
+
   const active = useAppSelector((state) => state.files.active);
   const hiddenList = useAppSelector((state) => state.files.hiddenList);
   const openedList = useAppSelector((state) => state.files.openedList);
   const globalZIndex = useAppSelector((state) => state.files.zIndex);
+
+  const [expanded, setExpanded] = useState(false);
+
+  const displayWidth = window.innerWidth;
+  const displayHeight = window.innerHeight;
+
+  const handleX = useMotionValue(200);
+  const handleY = useMotionValue(400);
+
+  const windowWidth = useTransform(handleX, [0, displayWidth], [0, displayWidth]);
+  const windowHeight = useTransform(handleY, [0, displayHeight], [0, displayHeight]);
+
+  const dragControls = useDragControls();
+  const resizeControls = useDragControls();
+
   const windowRef = useRef<HTMLDivElement>(null);
-
-  const controls = useDragControls();
-
   const setCurrentFileActive = () => {
     dispatch(setFileActive(item.id));
     if (windowRef.current) {
@@ -39,13 +51,53 @@ export default function Window({ item }: { item: Item }) {
           expanded ? 'expanded' : ''
         )}
         drag
-        dragControls={controls}
+        dragControls={dragControls}
         dragListener={false}
         dragTransition={{ power: 0 }}
         onTapStart={() => setCurrentFileActive()}
         ref={windowRef}
+        style={{ height: windowHeight, width: windowWidth }}
       >
-        <TitleBar controls={controls} expanded={expanded} item={item} setExpanded={setExpanded} />
+        <motion.div
+          className={styles.top}
+          drag="y"
+          dragConstraints={{ bottom: displayHeight, top: 0 }}
+          dragControls={resizeControls}
+          dragElastic={0}
+          style={{ touchAction: 'none', y: handleY }}
+        />
+        <motion.div
+          className={styles.left}
+          drag="x"
+          dragConstraints={{ left: 120, right: displayWidth }}
+          dragControls={resizeControls}
+          dragElastic={0}
+          style={{ touchAction: 'none', x: handleX }}
+        />
+        <motion.div
+          className={styles.right}
+          drag="x"
+          dragConstraints={{ left: 120, right: displayWidth }}
+          dragControls={resizeControls}
+          dragElastic={0}
+          style={{ touchAction: 'none', x: handleX }}
+        />
+        <motion.div
+          className={styles.bottom}
+          drag="y"
+          dragConstraints={{ bottom: displayHeight, top: 0 }}
+          dragControls={resizeControls}
+          dragElastic={0}
+          style={{ touchAction: 'none', y: handleY }}
+        />
+
+        <TitleBar
+          controls={dragControls}
+          expanded={expanded}
+          item={item}
+          setExpanded={setExpanded}
+        />
+
         <div className={styles.content}>
           {item.type === 'folder' && item.files
             ? item.files.map((a) => <Label item={a} key={a.id} />)
