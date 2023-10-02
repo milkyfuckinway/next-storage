3f3727ad2d47c4d6e774b04042c186406eb4d90b
milkyfuckinway <0-0_0@bk.ru>
2023-10-02 18:09:50 +0300

    fix

--- src/features/Window/WindowButton.tsx
+++ src/features/Window/WindowButton.tsx
@@ -1,13 +1,26 @@
 import { CloseIcon, CollapseIcon, ExpandIcon } from '@/assets/svg/index.svg';
+import clsx from 'clsx';
 
 import styles from './WindowButton.module.scss';
 
 export default function WindowButton({ onClick, type }: { onClick: () => void; type: string }) {
   return (
     <button className={styles.button} onClick={onClick} type="button">
-      {type === 'collapse' && <CollapseIcon className={styles.collapse} />}
-      {type === 'expand' && <ExpandIcon />}
-      {type === 'close' && <CloseIcon />}
+      {type === 'collapse' && (
+        <div className={clsx(styles.icon, styles.collapse)}>
+          <CollapseIcon />
+        </div>
+      )}
+      {type === 'expand' && (
+        <div className={clsx(styles.icon, styles.expand)}>
+          <ExpandIcon />
+        </div>
+      )}
+      {type === 'close' && (
+        <div className={clsx(styles.icon, styles.close)}>
+          <CloseIcon />
+        </div>
+      )}
     </button>
   );
 }
