dcfd26d5823ae7ee0e55e7eed887e72e0bc5c3ac
milkyfuckinway <0-0_0@bk.ru>
2023-10-06 15:28:12 +0300

    fix

--- src/features/Applications/InternetExplorer.tsx
+++ src/features/Applications/InternetExplorer.tsx
@@ -3,7 +3,12 @@ import styles from './InternetExplorer.module.scss';
 export default function InternetExplorer() {
   return (
     <div className={styles.content}>
-      <p>Internet explorer</p>
+      <iframe
+        height="100%"
+        src="https://www.google.com/search?igu=1"
+        title="homepage"
+        width="100%"
+      />
     </div>
   );
 }
