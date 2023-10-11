760207c2befea40a6fb57eb4905cd2855fff71f5
milkyfuckinway <0-0_0@bk.ru>
2023-10-11 19:51:16 +0300

    fix

--- src/features/Applications/ColorSettings.tsx
+++ src/features/Applications/ColorSettings.tsx
@@ -1,21 +1,32 @@
-import Label from '../Desktop/Label';
+import { useAppSelector } from '@/store/store';
+import convertStringToUrl from '@/utils/helpers';
+
 import styles from './ColorSettings.module.scss';
 
-const item = {
-  content: { link: '', paragraph: '' },
-  icon: 'folder',
-  id: 'example',
-  name: 'Название папки',
-  path: '/',
-  type: 'text',
-} as DesktopFile;
-
 export default function ColorSettings() {
+  const backgroundColor = useAppSelector((state) => state.wallpaper.color);
+  const backgroundImage = useAppSelector((state) => state.wallpaper.image);
+  const backgroundPosition = useAppSelector((state) => state.wallpaper.position);
+  const backgroundRepeat = useAppSelector((state) => state.wallpaper.repeat);
+  const backgroundSize = useAppSelector((state) => state.wallpaper.size);
+
   return (
     <div className={styles.content}>
       <div className={styles.display}>
-        <div className={styles.display__content}>
-          <Label item={item} />
+        <div
+          className={styles.display__content}
+          style={{
+            backgroundColor,
+            backgroundImage: convertStringToUrl(backgroundImage),
+            backgroundPosition,
+            backgroundRepeat,
+            backgroundSize,
+          }}
+        >
+          <button className={styles.label} type="button">
+            <div className={styles.label__icon} />
+            <div className={styles.label__name}>Имя файла</div>
+          </button>
         </div>
       </div>
     </div>
