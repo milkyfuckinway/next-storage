47543ad34578578138fbc6baa4022c6a9e7c6c36
milkyfuckinway <0-0_0@bk.ru>
2023-10-11 18:26:17 +0300

    fix

--- src/features/Applications/ColorSettings.tsx
+++ src/features/Applications/ColorSettings.tsx
@@ -1,5 +1,23 @@
+import Label from '../Desktop/Label';
 import styles from './ColorSettings.module.scss';
 
+const item = {
+  content: { link: '', paragraph: '' },
+  icon: 'folder',
+  id: 'example',
+  name: 'Название папки',
+  path: '/',
+  type: 'text',
+} as DesktopFile;
+
 export default function ColorSettings() {
-  return <div className={styles.content}>settings color</div>;
+  return (
+    <div className={styles.content}>
+      <div className={styles.display}>
+        <div className={styles.display__content}>
+          <Label item={item} />
+        </div>
+      </div>
+    </div>
+  );
 }
