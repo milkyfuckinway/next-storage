34f9095f8e71bb39624c792ac7d8d4e6c1321c28
milkyfuckinway <0-0_0@bk.ru>
2023-10-13 19:49:57 +0300

    fix

--- src/features/Text/WindowText.tsx
+++ src/features/Text/WindowText.tsx
@@ -1,6 +1,6 @@
 import styles from './WindowText.module.scss';
 
-export default function WindowText({ item }: { item: TextFile }) {
+export default function WindowText({ item }: { item: DocumentFile }) {
   return (
     <div className={styles.content}>
       <a href={item.content?.link}>{item.content.link}</a>
