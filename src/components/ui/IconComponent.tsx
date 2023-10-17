0c600d70ea998dc0272e5b36601df91d924f6533
milkyfuckinway <0-0_0@bk.ru>
2023-10-17 13:25:18 +0300

    fix

--- src/components/ui/IconComponent.tsx
+++ src/components/ui/IconComponent.tsx
@@ -2,30 +2,39 @@ import iconPaths from '@/shared/constants/IconPaths';
 import { useAppSelector } from '@/shared/store/store';
 import Image from 'next/image';
 
-export default function IconComponent({
+type Size = 'big' | 'medium' | 'small';
+
+const calcSize = (size: Size) => (size === 'small' ? 16 : 48);
+
+const calcPath = ({
   item,
+  openedList,
   size,
 }: {
   item: DesktopFile;
-  size: 'big' | 'medium' | 'small';
-}) {
+  openedList: DesktopFile[];
+  size: Size;
+}) => {
+  if (item.type === 'image') {
+    return item.src;
+  }
+
+  if (openedList.includes(item)) {
+    return size === 'small' ? iconPaths[item.icon].opened.small : iconPaths[item.icon].opened.big;
+  }
+  return size === 'small' ? iconPaths[item.icon].closed.small : iconPaths[item.icon].closed.big;
+};
+
+export default function IconComponent({ item, size }: { item: DesktopFile; size: Size }) {
   const openedList = useAppSelector((state) => state.files.openedList);
 
-  const calcPath = () => {
-    if (openedList.includes(item)) {
-      return size === 'small' ? iconPaths[item.icon].opened.small : iconPaths[item.icon].opened.big;
-    }
-    return size === 'small' ? iconPaths[item.icon].closed.small : iconPaths[item.icon].closed.big;
-  };
-
-  const calcSize = () => (size === 'small' ? 16 : 48);
-
-  if (item.type !== 'image') {
-    return (
-      <Image alt={item.type} height={calcSize()} priority src={calcPath()} width={calcSize()} />
-    );
-  }
-  if (item.type === 'image') {
-    return <Image alt={item.type} height={calcSize()} priority src={item.src} width={calcSize()} />;
-  }
+  return (
+    <Image
+      alt={item.type}
+      height={calcSize(size)}
+      priority
+      src={calcPath({ item, openedList, size })}
+      width={calcSize(size)}
+    />
+  );
 }
